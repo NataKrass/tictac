f54119d4945448cd6d8b2eaae3e061b77e6b8d72
Nata <natalikrasnobaieva@gmail.com>
2020-10-26 16:24:34 +0200

    sass added

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,18 +3,18 @@ import Square from "./Square"
 
 const Board = () => {
     return (
-        <div>
-            <div>
+        <div className="board">
+            <div className="board-row">
                 <Square value={0} />
                 <Square value={1}/>
                 <Square value={1}/>
             </div>
-            <div>
+            <div className="board-row">
                 <Square value={1}/>
                 <Square value={0}/>
                 <Square value={0}/>
             </div>
-            <div>
+            <div className="board-row">
                 <Square value={1}/>
                 <Square value={0}/>
                 <Square value={1}/>
