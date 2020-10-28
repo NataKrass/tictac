3c43c2f407f5bb8f42978b1e0f51abf183c4c2f6
Nata <natalikrasnobaieva@gmail.com>
2020-10-28 18:39:25 +0200

    button for history

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import Board from "./components/Board";
-import {calculateWinner} from "./helpers";
-import "./styles/root.scss";
+import React, { useState } from 'react';
+import Board from './components/Board';
+import History from './components/History';
+import { calculateWinner } from './helpers';
+import './styles/root.scss';
 
 const App = () => {
   const [history, setHistory] = useState([
-    {board: Array(9).fill(null),
-    isXNext: true}
-  ])
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
 
   const [currentMove, setCurrentMove] = useState(0);
 
@@ -17,27 +17,33 @@ const App = () => {
 
   const winner = calculateWinner(current.board);
 
-  const message = winner ? `Winner is ${winner}!!!` : `Next player is ${current.isXNext ? '0' : 'X'}`
+  const message = winner
+    ? `Winner is ${winner}!!!`
+    : `Next player is ${current.isXNext ? '0' : 'X'}`;
 
   const handleSquareClick = position => {
-    if (current.board[position] || winner ) {
+    if (current.board[position] || winner) {
       return;
     }
     setHistory(prev => {
-
-        const last = prev[prev.length - 1];
+      const last = prev[prev.length - 1];
 
       const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
+          console.log(history);
           return last.isXNext ? '0' : 'X';
         }
         return square;
       });
 
-      return prev.concat({ board: newBoard, isXNext: !last.isXNext })
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
-   
+
     setCurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
   };
 
   return (
@@ -45,6 +51,7 @@ const App = () => {
       <h1>Tic Tac Toe!</h1>
       <h3>{message}</h3>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
